4a92f62562db4a627c7468f4c2fb3868ed0969c7
all-laugh <xq264@nyu.edu>
2021-03-08 16:42:40 -0500

    Initial Commit

---
+++ Memory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Xiao Quan on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
