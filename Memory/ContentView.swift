089fee88d7797535baf481b43726031ae3cb50fc
all-laugh <xq264@nyu.edu>
2021-03-08 17:15:57 -0500

    Reconstructs project from lecture

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojiMemorizeGame: EmojiMemoryGame
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(emojiMemorizeGame.cards) { card in
+                CardView(card: card).onTapGesture {
+                    emojiMemorizeGame.choose(card: card)
+                }
+            }
+        }
+        .foregroundColor(.orange)
+        .font(Font.largeTitle)
+        .padding()
+        
     }
 }
 
+struct CardView: View {
+    var card: MemorizeGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content).foregroundColor(.black)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(emojiMemorizeGame: EmojiMemoryGame())
     }
 }
