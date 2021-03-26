c95fe3c9745b5d67f15fb8e2a37758dd4c7b9006
all-laugh <xq264@nyu.edu>
2021-03-26 14:01:26 -0400

    up to lecture 5

--- Memory/EmojiMemoryGameView.swift
+++ Memory/EmojiMemoryGameView.swift
@@ -21,67 +21,66 @@ struct EmojiMemoryGameView: View {
                 .fontWeight(.bold)
                 .padding(.top, 10)
                 .foregroundColor(themeColor)
-                .font(.headline)
+                .font(.title)
             Text("Score: \(score)").font(.caption)
-        }
-
-        
-        Grid(emojiMemorizeGame.cards) { card in
-                    CardView(card: card).onTapGesture {
-                        emojiMemorizeGame.choose(card)
-                    }
-                    .padding(2)
-        }
-        .foregroundColor(themeColor)
-//        .font(Font.largeTitle)
-        .padding()
-        
-        Button("New Game") {
-            self.emojiMemorizeGame.resetModel()
-        }
-        .padding(.bottom, 15)
-        .foregroundColor(themeColor)
-        .font(.headline)
-    }
-}
-
-
-struct CardView: View {
-    var card: MemorizeGame<String>.Card
-    
-    var body: some View {
-        GeometryReader { geometry in
-            self.body(for: geometry.size)
-        }
-    }
-    
-    @ViewBuilder
-    func body (for size: CGSize) -> some View {
-        if card.isFaceUp || !card.isMatched {
-            ZStack {
-                CountdownPie(
-                    startAngle: Angle(degrees: 0-90),
-                    endAngle: Angle(degrees: 110-90)
-                )
-                .padding(5)
-                .opacity(0.4)
-                Text(card.content)
+            
+            Grid(emojiMemorizeGame.cards) { card in
+                        CardView(card: card).onTapGesture {
+                            emojiMemorizeGame.choose(card)
+                        }
+                        .padding(2)
             }
-            .cardify(isFaceUp: card.isFaceUp)
-            .font(Font.system(size: font(for: size)))
+            .foregroundColor(themeColor)
+            .padding(5)
+            
+            Button("New Game") {
+                self.emojiMemorizeGame.resetModel()
+            }
+            .padding(.bottom, 15)
+            .foregroundColor(themeColor)
+            .font(.body)
+        }
+
+    }
+
+
+    struct CardView: View {
+        var card: MemorizeGame<String>.Card
+        
+        var body: some View {
+            GeometryReader { geometry in
+                self.body(for: geometry.size)
+            }
+        }
+        
+        @ViewBuilder
+        func body (for size: CGSize) -> some View {
+            if card.isFaceUp || !card.isMatched {
+                ZStack {
+                    CountdownPie(
+                        startAngle: Angle(degrees: 0-90),
+                        endAngle: Angle(degrees: 110-90)
+                    )
+                    .padding(5)
+                    .opacity(0.4)
+                    Text(card.content)
+                }
+                .cardify(isFaceUp: card.isFaceUp)
+                .font(Font.system(size: font(for: size)))
+            }
+        }
+        
+        // MARK: - Drawing Constants
+        private func font(for size: CGSize) -> CGFloat {
+            min(size.width, size.height ) * 0.65
         }
     }
-    
-    // MARK: - Drawing Constants
-    private func font(for size: CGSize) -> CGFloat {
-        min(size.width, size.height ) * 0.65
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        let game = EmojiMemoryGame()
-        game.choose(game.cards[0])
-        return EmojiMemoryGameView(emojiMemorizeGame: game)
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            let game = EmojiMemoryGame()
+            game.choose(game.cards[0])
+            return EmojiMemoryGameView(emojiMemorizeGame: game)
+        }
     }
 }
